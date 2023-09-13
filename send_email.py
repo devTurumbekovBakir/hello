c3ee81d4837f8e7b0281ffda5552b6ce0ccee011
turumbekov bakir <bakirturumbekov34@gmail.com>
2023-09-13 11:45:01 +0600

    added addresses to mes

--- send_email.py
+++ send_email.py
@@ -1,3 +1,8 @@
 def send_mail(text, address, title):
     message = text.format()
     return message + address + title
+
+
+def get_address(filepath):
+    with open(filepath, 'r') as f:
+        return f.read()
